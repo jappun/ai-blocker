9b886db75bbfdb6b54a3b1c7bc043f84f3016fb3
jappun <jappun@student.ubc.ca>
2025-03-23 01:01:57 -0700

    basic styles done

--- blocker.js
+++ blocker.js
@@ -4,15 +4,19 @@ function block() {
     const container = document.createElement("div");
     container.id = "extension-container";
 
-    // container.style.position = "fixed";
-    // container.style.top = "0";
-    // container.style.left = "0";
-    // container.style.width = "100%";
-    // container.style.height = "100%";
-    // container.style.zIndex = "9999";
+    container.style.position = "fixed";
+    container.style.top = "0";
+    container.style.left = "0";
+    container.style.width = "100vw";
+    container.style.height = "100vh";
+    container.style.zIndex = "9999";
+    container.style.display = "flex";
+    container.style.alignItems = "center";
+    container.style.justifyContent = "center";
+    container.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
 
     document.body.appendChild(container);
-    const shadowDOM = container.attachShadow({ mode: "closed" });
+    const shadowDOM = container.attachShadow({ mode: "open" });
 
     const blocker = chrome.runtime.getURL("blocker.html");
     const styles = chrome.runtime.getURL("styles.css");
